7bdaef5aede3ea359c3862c1ba9d6c2e6f50803c
Johannes Englisch <englisch@shh.mpg.de>
2021-07-23 12:06:55 +0200

    fixed tests

--- test.py
+++ test.py
@@ -3,12 +3,12 @@ def test_valid(cldf_dataset, cldf_logger):
 
 
 def test_forms(cldf_dataset):
-    assert len(list(cldf_dataset["FormTable"])) == 7317
+    assert len(list(cldf_dataset["FormTable"])) == 7621
     assert any(f["Form"] == "h a k i ʔ i" for f in cldf_dataset["FormTable"])
 
 
 def test_parameters(cldf_dataset):
-    assert len(list(cldf_dataset["ParameterTable"])) == 234
+    assert len(list(cldf_dataset["ParameterTable"])) == 244
 
 
 def test_languages(cldf_dataset):
